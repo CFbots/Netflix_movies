f39b468c40b41a3ed849e793650d048ae50b2442
Chi Feng <chifengiris@gmail.com>
2023-05-02 00:30:07 -0700

    create homepage & navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageBackgroundComponent } from './homepage-background/homepage-background.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HomepageBackgroundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
