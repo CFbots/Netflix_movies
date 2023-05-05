a6a81a4676c5aaa1a56704860b9a925393f2cc75
Chi Feng <chifengiris@gmail.com>
2023-05-05 16:58:31 -0700

    add match comfirm password validator and adjust UI

--- src/app/register-first/register-first.component.ts
+++ src/app/register-first/register-first.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-first',
@@ -12,9 +12,11 @@ export class RegisterFirstComponent implements OnInit{
   ngOnInit(): void {
     this.registerForm_1 = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]]
-    })
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]], 
+      confirm:['']
+    }, { validators: this.matchPassword })
   }
+
   get email() {
     return this.registerForm_1.get('email');
   }
@@ -22,6 +24,19 @@ export class RegisterFirstComponent implements OnInit{
   get password() {
     return this.registerForm_1.get('password');
   }
+
+  get comfirm(){
+    return this.registerForm_1.get('confirm');
+  }
+
+  matchPassword: ValidatorFn = (group: AbstractControl): ValidationErrors | null =>{
+    console.log("hhihi")
+    const password = group.get("password")?.value;
+    const confirm = group.get("confirm")?.value;
+    if (password != confirm) { console.log("not match!"); return { 'noMatch': true } }
+    return null
+  }
+
   onSubmit() {
     console.log(this.registerForm_1.value);
   }
